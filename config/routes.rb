1a05747448a0c7668c36207bbe1a99af7b7bdba2
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2017-09-18 02:52:12 -0400

    added song list view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'song/list'
+  get 'song/new'
+  post 'song/create'
+  patch 'song/update'
+  get 'song/list'
+  get 'song/show'
+  get 'song/edit'
+  get 'song/delete'
+  get 'song/update'
+  get 'book/show_subjects'
 end
