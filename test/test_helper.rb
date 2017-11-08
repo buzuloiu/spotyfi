bd10e5c2f76dcb7a67f4225b884635ea912b7388
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2017-11-08 00:32:11 -0500

    fixed tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :users
-  fixtures :songs
     self.set_fixture_class users: User
 
 
