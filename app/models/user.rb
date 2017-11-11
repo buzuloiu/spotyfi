29ae97f2cd3f19b20ce8dc27253df4ef181d9479
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-11-11 00:00:31 -0500

    end of user model tests for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   before_save { self.email = email.downcase }
   validates :name, presence:true, length:{maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,5 +7,5 @@ class User < ApplicationRecord
                                 format: {with: VALID_EMAIL_REGEX},
                                 uniqueness: true
 
-  #has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,9 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = users(:valid)
+    #@user = users(:one)
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -51,6 +53,23 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email should be lowecase before saving" do
+    mixed_case_email = "DaVe@GmaiL.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal @user.reload.email, mixed_case_email.downcase
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
