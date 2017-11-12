bbe1e697d477847a2900dced54a4824c01f88c18
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-11-12 15:44:39 -0500

    added user sign up, still missing successful signup test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
   before_save { self.email = email.downcase }
   validates :name, presence:true, length:{maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +7,6 @@ class User < ApplicationRecord
                                 uniqueness: true
 
   validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+
 end
