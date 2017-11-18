69fd5989f7babb04c1c32ded10e8ac81c9c6b663
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-11-17 20:14:58 -0500

    log in/out works now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  end
+      log_out
+      redirect_to root_url
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence:true, length:{maximum: 50}
@@ -8,5 +10,12 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
 end
