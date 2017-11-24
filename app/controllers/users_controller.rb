bc7c0e65869bd3bdc359ea3de9f9897116ba6291
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-11-24 00:08:36 -0500

    seeing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
 
 
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:paul)
+    @other_user = users(:steve)
+
   test "should get new" do
     get users_new_url
     assert_response :success
+  end
+
+  test "should go to index when not logged in" do
+    get users_path
+    assert redirect_to login_url
   end
 
 end
