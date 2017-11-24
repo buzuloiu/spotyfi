ea8acc102aa5e59fdc542f6a35fb66cd1704bf4a
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-11-24 00:45:57 -0500

    adding admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118023231) do
+ActiveRecord::Schema.define(version: 20171124052929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20171118023231) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:paul)
     @other_user = users(:steve)
+  end
 
   test "should get new" do
     get users_new_url
@@ -13,7 +14,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should go to index when not logged in" do
     get users_path
-    assert redirect_to login_url
+    assert_redirected_to login_url
   end
 
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
