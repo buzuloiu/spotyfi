1756003a0eeaabfb0aca1f527377045889926097
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-12-03 23:44:44 -0500

    added reset digest and reset controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128024002) do
+ActiveRecord::Schema.define(version: 20171204044350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20171128024002) do
     t.boolean "activated", default: false
     t.string "activation_digest"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
