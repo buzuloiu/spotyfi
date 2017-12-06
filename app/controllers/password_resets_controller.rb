5a8457fe740e73372f3e39309fcd0557200fcc2b
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-12-06 00:47:03 -0500

    finishing up password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -17,4 +21,48 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
-end
+
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+            render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+        @user = User.find_by(email: params[:email])
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? &&
+                @user.authenticated?(:reset, params[:id]))
+                redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      #check if a password reset url is expired
+      if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+      end
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  def password_reset_expired?
+   reset_sent_at < 2.hours.ago
+  end
+
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -42,23 +46,25 @@ class User < ApplicationRecord
   end
 
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
 
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   private
 
-    def create_reset_digest
-      self.reset_token = User.new_token
-      update_attribute(:reset_digest,  User.digest(reset_token))
-      update_attribute(:reset_sent_at, Time.zone.now)
-    end
 
-    def send_password_reset_email
-      UserMailer.password_reset(self).deliver_now
-    end
-    
+
+
     def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
