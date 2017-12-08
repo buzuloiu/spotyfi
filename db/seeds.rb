e0730be48c084883212638355a8ce2fc3ede51de
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-12-07 22:51:56 -0500

    micropost controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,9 @@ User.create!(name:  "Paul Test",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::FamilyGuy.quote
+  users.each { |user| user.microposts.create!(content: content) }
+end
