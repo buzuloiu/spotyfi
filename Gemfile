637d0246cb6c7e867a74b176674681cfaa2a3314
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-07-20 19:23:21 -0400

    more mail

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'bootstrap-will_paginate'
 
 gem 'rest-client'
 
+gem 'mailgun-ruby'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.delivery_method = :http
     host = 'spotyfi.herokuapp.com'
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
@@ -64,7 +64,7 @@ Rails.application.configure do
       :enable_starttls_auto => true
     }
 
-  
+
 
 
 
