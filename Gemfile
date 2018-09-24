f1ef466c6a1bfc7701aa363825932426b530ee44
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-09-24 16:42:43 -0400

    more mail

--- Gemfile
+++ Gemfile
@@ -61,6 +61,8 @@ gem 'rest-client'
 
 gem 'mailgun-ruby'
 
+gem "letter_opener", :group => :development
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,9 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
