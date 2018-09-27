ad6529c9f1232da82cce15999e4314e49dc40308
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-09-27 13:32:14 -0400

    even more mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
 
 
-
+  config.action_mailer.default_url_options = { :host => "www.yourhost.com" }
   config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
