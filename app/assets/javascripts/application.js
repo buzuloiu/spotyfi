fc2f24c2d36a6eb3efa398bdeb830f1a6a623b33
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-11-27 15:19:05 -0500

    redundancies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require bootstrap
-//= require jquery_ujs
-//= require jquery-ui
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
